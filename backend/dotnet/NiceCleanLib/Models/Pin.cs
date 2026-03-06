ce4a70b6081596668a3b3c02cfbdb82f7739a83b
Patrick <pfriis@outlook.com>
2026-03-06 20:28:44 +0100

    Refactor Pin.cs to implement enums

--- backend/dotnet/NiceCleanLib/Models/Pin.cs
+++ backend/dotnet/NiceCleanLib/Models/Pin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NiceCleanLib.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,19 +10,19 @@ public class Pin
     public int PinId { get; }
     public DateTime Date { get; set; }
     public string Name { get; set; }
-    public string Severity { get; set; }
-    public string PollutionType { get; set; }
+    public PollutionSeverity Severity { get; set; }
+    public PollutionType PollutionType { get; set; }
     public int Radius { get; set; }
-    public string Status { get; set; }
+    public PinStatus Status { get; set; }
 
-    public Pin(int pinId, DateTime date, string name, string pollutionSeverity, string pollutionType, int radius, string pinStatus)
+    public Pin(int pinId, DateTime date, string name, PollutionSeverity severity, PollutionType pollutionType, int radius, PinStatus status)
     {
         PinId = pinId;
         Date = date;
         Name = name;
-        Severity = pollutionSeverity;
+        Severity = severity;
         PollutionType = pollutionType;
         Radius = radius;
-        Status = pinStatus;
+        Status = status;
     }
 }
