dab98275ec8fa915fd9efaf56d9db8f6da9239e4
Patrick <pfriis@outlook.com>
2026-03-06 20:06:36 +0100

    Add Pin class

---
+++ backend/dotnet/NiceCleanLib/Models/Pin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NiceCleanLib.Models;
+
+public class Pin
+{
+    public int PinId { get; }
+    public DateTime Date { get; set; }
+    public string Name { get; set; }
+    public string Severity { get; set; }
+    public string PinType { get; set; }
+    public int Radius { get; set; }
+    public string PinStatus { get; set; }
+
+    public Pin(int pinId, DateTime date, string name, string severity, string pinType, int radius, string pinStatus)
+    {
+        PinId = pinId;
+        Date = date;
+        Name = name;
+        Severity = severity;
+        PinType = pinType;
+        Radius = radius;
+        PinStatus = pinStatus;
+    }
+}
