91f9a7841bfb97b715b3c4f730aed802655cc046
Patrick <pfriis@outlook.com>
2026-03-12 13:17:19 +0100

    Add Contracts DTO for PinCreation

---
+++ backend/dotnet/NiceCleanREST/Contracts/PinCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿using NiceCleanLib.Enums;
+
+namespace NiceCleanREST.Contracts;
+
+public class PinCreateDTO
+{
+    public string Name { get; set; } = string.Empty;
+    public PollutionSeverity Severity { get; set; }
+    public PollutionType PollutionType { get; set; }
+    public double Latatitude { get; set; }
+    public double Longitude { get; set; }
+}
