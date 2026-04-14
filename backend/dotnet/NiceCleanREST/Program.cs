3d4464eac85c1cdc691ea7c8b491278bf98a4c57
Patrick <pfriis@outlook.com>
2026-04-14 19:22:31 +0200

    Add JSON StringEnumConverter to serialize enums as strings

--- backend/dotnet/NiceCleanREST/Program.cs
+++ backend/dotnet/NiceCleanREST/Program.cs
@@ -2,12 +2,18 @@ using Microsoft.EntityFrameworkCore;
 using NiceCleanLib.Data;
 using NiceCleanLib.Services.Interfaces;
 using NiceCleanLib.Services.Repositories;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
